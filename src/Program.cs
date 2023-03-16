7db1ee7d57cd68babd67c0ffafb8b2b895600d96
Bryan Gruneberg <hello@bryangruneberg.com>
2023-03-16 14:42:00 -0600

    Adds an API

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
 
 app.MapGet("/", (HttpContext context) => {
 
@@ -26,4 +27,32 @@ app.MapGet("/", (HttpContext context) => {
     return words;
 });
 
+app.MapGet("/api", (HttpContext context) => {
+  
+    var words = @"amazee.io rocks you";
+
+    var path = "/app/api.words.txt";
+    if (File.Exists(path)) {
+        words = System.IO.File.ReadAllText(path);
+    }
+
+    var overridePath = "/app/words/api.words.override.txt";
+    if (File.Exists(overridePath)) {
+        words = System.IO.File.ReadAllText(overridePath);
+    }
+
+    context.Response.Headers["X-LAGOON"] = Environment.GetEnvironmentVariable("HOSTNAME");
+
+    var OWords = new WordyWords { words = words };
+
+    string jsonString = JsonSerializer.Serialize(OWords);
+
+    return jsonString;
+});
+
 app.Run();
+
+class WordyWords
+{
+    public string? words { get; set; }
+}
